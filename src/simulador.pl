3681f04a653899267609215a40e62ec8ddfc36f7
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2020-11-21 13:40:36 -0300

    agregar tareas

---
+++ src/simulador.pl
@@ -0,0 +1,14 @@
+# // Fran:
+# // 1. Implementar DB, Implementar Escritor, Implementar Lector
+# // 2. Armar semaforos
+# // 3. Sincronizar todo
+
+# // Marce:
+# // 1. Implementar TDA cola
+# // 2. Implementar FIFO en el planificador
+# // 3. Armar el dispatcher
+
+# // Despues:
+# // 1. Lanzar procesos hardcodeados
+# // 2. Agregar dinamicamente procesos
+# // 3. Monitorear
